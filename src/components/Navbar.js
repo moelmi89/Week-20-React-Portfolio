ccf3eb8535e25e11831923f782e6eb11ccb21f43
Moelmi89 <moelmi89@gmail.com>
2023-03-09 23:40:09 +0000

    navbar updated button js added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,45 @@ function Navbar () {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
-    <nav className="navbar">
-        <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                // Name Goes HERE
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-                <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+     <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    // Name Goes HERE
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                        <li className="nav-item">
+                        <Link to="/projects" className="nav-links" onClick={closeMobileMenu}>
+                            Projects
+                        </Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/resume" className="nav-links" onClick={closeMobileMenu}>
+                            Resume
+                        </Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+                            Contact
+                        </Link>
+                        </li>
+                    </li>
+                </ul>
             </div>
-        </div>
-    </nav>
-    )
+        </nav>
+     </>   
+   )
+
 }
 
 export default Navbar
