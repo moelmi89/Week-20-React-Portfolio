4a4d0b824c97954706fee6bec4bcc08bcde0dbee
Moelmi89 <moelmi89@gmail.com>
2023-03-13 00:35:58 +0000

    App js files added along with styling

---
+++ src/Constants.js
@@ -0,0 +1,55 @@
+import TEXTEDITOR from "./Assets/Projects/pwatexteditor.png";
+import TASTY from "./Assets/Projects/tasty.png";
+import ECOMERCE from "./Assets/Projects/backend.png";
+import WEATHER from "./Assets/Projects/weather.png";
+import PLANNER from "./Assets/Projects/planner.png";
+import TECHBLOG from "./Assets/Projects/techblog.png";
+
+
+export const PROJECTS = [
+  {
+    image: WEATHER,
+    name: "Weather Dashboard",
+    technologyUsed: "Javascript, Server Side API's, HTML, CSS",
+    description: "A weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.",
+    url: "https://moelmi89.github.io/Week-6-Third-Party-API-Weather-Dashboard/",
+  },
+  {
+    image: TECHBLOG,
+    name: "Tech Blog",
+    technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
+    description: "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
+    url: "https://code-blog.herokuapp.com/",
+  },
+  {
+    image: TASTY,
+    name: "TASTY",
+    technologyUsed: "ReactNative, Javascript, Redux",
+    description: "Team collaboration mobile app and online services like slack Message people and groups without sharing phone numbers, communicate with managers and staff all in one place. and discuss or assign tasks to get stuff done",
+    url: "https://connielau123.github.io/TASTY-restaurant-finder-application/",
+  },
+  {
+    image: TEXTEDITOR,
+    name: "PWA Text Editor",
+    technologyUsed: "ReactJs, Typescript, Redux",
+    description: "The next-gen Robotic Process Automation (#RPA) platform for retail and telecom domains. The ultimate automation platform for your Shopify, Email, E- commerce shop",
+    url: "https://floating-brook-08559.herokuapp.com/",
+  },
+  {
+    image: ECOMERCE,
+    name: "E-commerce Back-end",
+    technologyUsed: "ReactNative, Javascript, Redux",
+    description: "Social media mobile app similar to Twitter for china. Social networking GUOMEDIA is an all-in-one social networking platform where users can build their own user profiles, create their own posts, Livestream broadcast and follow each other",
+    url: "https://play.google.com/store/apps/details?id=com.tiu.guo.media",
+  },
+  {
+    image: PLANNER,
+    name: "Work Day Scheduler",
+    technologyUsed: "ReactJs, Typescript, Redux",
+    description: "TechSpec is e-commerce web app. User can compare his product and purchange. This had a dashboard web app which had the data of orders placed and type of user.",
+    url: "https://moelmi89.github.io/Week-5-Third-Party-API-Calender/",
+  },
+
+];
+
+export const TOOLS = ["Visual Studio Code", "Git", "Chrome DevTools", "Npm (Node Package Manager)", "Atom", "Sublime Text", "Linux", "Heroku", "Postman"]
